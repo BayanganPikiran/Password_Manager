102d612a74b562fdf8f9a5e2e99fee4aa42ed459
Andre LaFlamme <bayangan.pikiran@gmail.com>
2023-05-12 08:41:41 +0700

    created Account class with initq

---
+++ accounts.py
@@ -0,0 +1,12 @@
+import random
+import tkinter as tk
+
+class Account:
+
+    def __init__(self):
+        self.accounts_dictionary = {}
+        self.new_account = {}
+        self.website = None
+        self.username = None
+        self.password = None
+
