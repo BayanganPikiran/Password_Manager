8e33464ebf6dddb2ef4f7874d2ef6d58e820a811
Andre LaFlamme <bayangan.pikiran@gmail.com>
2023-05-17 10:46:42 +0700

    switched plans; using sqlite; adjusted accounts to reflect change

--- accounts.py
+++ accounts.py
@@ -1,25 +1,35 @@
+import sqlite3
 import random
-import tkinter as tk
 
 
 class Account:
 
-    def __init__(self):
-        self.accounts_dictionary = {}
-        self.new_account = {}
-        self.website = None
-        self.username = None
-        self.password = None
-        self.up_cap_letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-                               "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-        self.low_cap_letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-                                "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-        self.numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-        self.symbols = []
+    def __init__(self, db_path):
+        self.database = db_path
+        self.conn = sqlite3.connect(self.database)
+        self.cursor = self.conn.cursor()
+        self.account_table = self.create_account_table()
 
-    def save_account(self, website, username, password):
-        account = {website: [username, password]}
-        self.accounts_dictionary.update(account)
+    def create_account_table(self):
+        account_table = self.cursor.execute("""CREATE TABLE accounts(
+            website text,
+            username text,
+            password text)""")
+        self.conn.commit()
+        self.conn.close()
+        return account_table
+
+    def create_record(self, site, user, password):
+        pass
+
+    def update_record(self):
+        pass
+
+    def fetch_record(self):
+        pass
+
+    def delete_record(self):
+        pass
 
     def generate_password(self):
         pass
