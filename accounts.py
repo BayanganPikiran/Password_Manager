fe741dad823944cd8ff3bbc3b41505b17b5fd604
Andre LaFlamme <bayangan.pikiran@gmail.com>
2023-05-25 12:21:21 +0700

    fixed create_records_table (was accounts tab)

--- accounts.py
+++ accounts.py
@@ -8,19 +8,27 @@ class Account:
         self.database = db_path
         self.conn = sqlite3.connect(self.database)
         self.cursor = self.conn.cursor()
-        self.account_table = self.create_accounts_table()
+        self.account_table = self.records_accounts_table()
 
-    def create_accounts_table(self):
-        accounts_table = self.cursor.execute("""CREATE TABLE IF NOT EXISTS accounts(
-            website text,
-            username text,
-            password text)""")
+    def record_already_exists(self, site, user):
+        is_record_query = "SELECT * FROM accounts WHERE EXISTS website=? AND username=?"
+        parameters = (site, user)
+        self.cursor.execute(is_record_query, parameters)
         self.conn.commit()
-        return accounts_table
 
-    def create_record(self, website, username, password):
-        self.cursor.execute("INSERT INTO accounts (website, username, password) VALUES (?, ?, ?)",
-                            (website, username, password))
+    def records_accounts_table(self):
+        records_table = self.cursor.execute("""CREATE TABLE IF NOT EXISTS accounts(
+            record_id INTEGER PRIMARY KEY, 
+            website TEXT NOT NULL,
+            username TEXT NOT NULL,
+            password TEXT NOT NULL)""")
+        self.conn.commit()
+        return records_table
+
+    def create_record(self, site, user, password):
+        create_record = "INSERT INTO accounts (website, username, password) VALUES (?, ?, ?)"
+        parameters = (site, user, password)
+        self.cursor.execute(create_record, parameters)
         self.conn.commit()
 
     def confirm_record_input(self):
@@ -44,17 +52,8 @@ class Account:
             password = str(item).strip("'()',")
             return password
 
-    def delete_record(self, site, username):
+    def delete_record(self, site, user):
         delete = "DELETE from accounts WHERE website=? AND username=?"
-        parameters = (site, username)
+        parameters = (site, user)
         self.cursor.execute(delete, parameters)
         self.conn.commit()
-
-    def generate_password(self):
-        pass
-
-    def retrieve_account(self):
-        pass
-
-    def delete_account(self):
-        pass
