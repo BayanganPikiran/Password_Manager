e573a5955eca37c1c595877c3f761b48b8fcb871
Andre LaFlamme <bayangan.pikiran@gmail.com>
2023-06-26 04:38:34 +0700

    added destroy() to update_pass toplevel so that toplevel destroys on button click

--- main.py
+++ main.py
@@ -134,7 +134,7 @@ class UI(Account):
         password.pack(expand=True, fill=tk.BOTH)
         confirm_btn = tk.Button(btn_frame, text="Update password",
                                 command=lambda: [self.update_record(pass_var, web_var, user_var),
-                                                 self.confirm_record_input()])
+                                                 self.confirm_record_input(), update_pass.destroy()])
         confirm_btn.pack(expand=True, fill=tk.BOTH)
 
     def create_save_record_btn(self):
